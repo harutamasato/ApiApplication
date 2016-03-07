659ea52c9ba3eb4a908f0cbe2bfaf180b40acf11
Masato Haruta <haruta@328w.co.jp>
2016-03-07 15:44:04 +0900

    add files

---
+++ app/src/main/java/com/toolbar/jp/apiapplication/ApiHoge.java
@@ -0,0 +1,12 @@
+package com.toolbar.jp.apiapplication;
+
+public class ApiHoge {
+    private String mHuga;
+    public ApiHoge(String huga) {
+        mHuga = huga;
+    }
+
+    public String getHuga() {
+        return mHuga;
+    }
+}
